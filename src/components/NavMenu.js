02d1e2fcae5478bdd0e5664ed647dfaeb18b8915
Stefano Corra <stefano.corra@hotmail.com>
2022-06-22 15:21:40 -0400

    first commit

---
+++ src/components/NavMenu.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const NavMenu = () => {
+  return (
+    <div>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Projects</li>
+        <li>Contact</li>
+      </ul>
+    </div>
+  )
+}
+
+export default NavMenu
