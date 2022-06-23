af7087a36378e1ca1b174f671b351161a0bf5f03
Stefano Corra <stefano.corra@hotmail.com>
2022-06-22 21:04:37 -0400

    navmenu commit

--- src/components/NavMenu.js
+++ src/components/NavMenu.js
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { NavLink } from 'react-router-dom'
 import styled from 'styled-components'
+import { MdMenu, MdClose } from 'react-icons/md'
 
 const NavMenuStyles = styled.div`
   position: fixed;
@@ -20,37 +21,141 @@ const NavMenuStyles = styled.div`
       border-radius: 8px;
       transition: 0.3s ease background-color;
       &:hover {
-        background-color: var(--deep-black);
+        background-color: var(--deep-dark);
       }
     }
     a {
       display: inline-block;
       font-family: 'RobotoMono Regular';
-      padding: 1rem 1rem;
+      padding: 1rem 2rem;
       font-size: 2rem;
       color: var(--gary-1);
       outline: none;
+    }
+    .active {
+      color: var(--white);
+    }
+  }
+  .mobile-menu-icon {
+    position: absolute;
+    right: 1rem;
+    top: 1rem;
+    width: 4rem;
+    cursor: pointer;
+    display: none;
+    outline: none;
+    * {
+      pointer-events: none;
+    }
+  }
+  .closeNavIcon {
+    display: none;
+  }
+
+  @media only screen and (max-width: 768px) {
+    padding: 0;
+    .hide-item {
+      display: none;
+      transform: translateY(calc(-100% - var(--top)));
+    }
+    .mobile-menu-icon {
+      display: block;
+    }
+    .navItems {
+      --top: 1rem;
+      transition: 0.3s ease transform;
+      background-color: var(--deep-dark);
+      padding: 2rem;
+      width: 90%;
+      max-width: 300px;
+      border-radius: 12px;
+      position: absolute;
+      right: 1rem;
+      top: var(--top);
+
+      .closeNavIcon {
+        display: block;
+        width: 3rem;
+        margin: 0 0 0 auto;
+        cursor: pointer;
+        * {
+          pointer-events: none;
+        }
+      }
+      li {
+        display: block;
+        margin-bottom: 1rem;
+      }
     }
   }
 `
 
 const NavMenu = () => {
+  const [showNav, SetShowNav] = useState(false)
   return (
     <NavMenuStyles>
-      <ul>
+      <div
+        className='mobile-menu-icon'
+        onClick={() => SetShowNav(!showNav)}
+        role='button'
+        onKeyDown={() => SetShowNav(!showNav)}
+        tabIndex={0}
+      >
+        <MdMenu />
+      </div>
+      <ul className={!showNav ? 'navItems hide-item' : 'navItems'}>
+        <div
+          className='closeNavIcon'
+          onClick={() => SetShowNav(!showNav)}
+          role='button'
+          onKeyDown={() => SetShowNav(!showNav)}
+          tabIndex={0}
+        >
+          <MdClose />
+        </div>
         <li>
-          <NavLink exact to='/'>
+          <NavLink
+            to='/'
+            onClick={() => SetShowNav(!showNav)}
+            role='button'
+            onKeyDown={() => SetShowNav(!showNav)}
+            tabIndex={0}
+          >
             Home
           </NavLink>
         </li>
         <li>
-          <NavLink to='/about'>About</NavLink>
+          <NavLink
+            to='/about'
+            onClick={() => SetShowNav(!showNav)}
+            role='button'
+            onKeyDown={() => SetShowNav(!showNav)}
+            tabIndex={0}
+          >
+            About
+          </NavLink>
         </li>
         <li>
-          <NavLink to='/projects'>Projects</NavLink>
+          <NavLink
+            to='/projects'
+            onClick={() => SetShowNav(!showNav)}
+            role='button'
+            onKeyDown={() => SetShowNav(!showNav)}
+            tabIndex={0}
+          >
+            Projects
+          </NavLink>
         </li>
         <li>
-          <NavLink to='/contact'>Contact</NavLink>
+          <NavLink
+            to='/contact'
+            onClick={() => SetShowNav(!showNav)}
+            role='button'
+            onKeyDown={() => SetShowNav(!showNav)}
+            tabIndex={0}
+          >
+            Contact
+          </NavLink>
         </li>
       </ul>
     </NavMenuStyles>
