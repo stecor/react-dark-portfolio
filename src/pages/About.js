23c091d466c0381650f9a1c9ab205102de929877
Stefano Corra <stefano.corra@hotmail.com>
2022-07-03 21:49:16 -0400

    commit

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,22 @@
 import React from 'react'
 
 const About = () => {
-  return <h1>This is About</h1>
+  return (
+    <p>
+      A proactive and organized individual with an Associate degree in Computer
+      Software and a background in web applications. As an enthusiastic and
+      creative technologist with hands-on skills in a wide range of software,
+      tools, and programming languages, positioned to leverage my abilities and
+      qualifications. Over the past 4 years, as a Developer, I have designed,
+      developed, and launched customized, highly responsive websites and
+      e-commerce solutions using advanced technologies. In addition, I have
+      consistently met client expectations and project milestones while working
+      effectively in deadline-driven environments. Please consider the following
+      highlights: Designed and developed webpages using jQuery, JavaScript,
+      HTML5, and CSS/Bootstrap, Built components in ReactJS using asynchronous
+      methods. Created e-commerce sites for small business owners.
+    </p>
+  )
 }
 
 export default About
